fb441277bdd111129302c426b19005fae1a1a741
PritamSavagavi <pritamsavagavi@gmail.com>
2024-05-17 12:44:12 +0530

    *NGFor basics

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,33 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-  product = {
-    name: 'iPhone',
-    price: 999,
-    color: 'Red',
-    discount: 78,
-    stock: 10,
-  };
-  name :string = 'John Carter';
-  mobileSrc: string = '/assets/Images/iphone-14-pro.png';
-  cartCounter : number = 0;
-
-  inStock: string | number =
-    this.product.stock > 0 ? this.product.stock.toString() : 'Not in stock';
-  getStockColor(): string {
-    return this.product.stock == 0 ? 'red' : 'inherit';
-  }
-  getDiscount() {
-    var discountedPrice = this.product.price - this.product.discount;
-  }
-  decCounter()
-  {
-    if(!(this.cartCounter<1))
-    this.cartCounter--;
-  }
-  incCounter()
-  {
-    if(this.cartCounter<this.product.stock)
-    this.cartCounter++;
-  }
+  listOfString : string[] = ['this','is','the','list','for','string'];
+  
 }
